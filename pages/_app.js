59bc444d2f76fcc02583e189a7285e8a4db437f4
kingstar1010 <thomashan1103@gmail.com>
2021-03-07 21:48:09 +0100

    before deploy

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,18 @@ import '../styles/navbar.css';
 import '../styles/sidebar.css';
 import '../styles/contact.css';
 import '../styles/about.css';
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
