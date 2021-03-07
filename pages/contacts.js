976beca2119338e06abd2b0ad555691ed012a47e
kingstar1010 <thomashan1103@gmail.com>
2021-03-07 21:16:41 +0100

    service id iopt

--- pages/contacts.js
+++ pages/contacts.js
@@ -1,19 +1,19 @@
-import NavBar from '../components/navbar/Navbar'
-import Contact from '../components/contact/contact'
-import ContactInfo from '../components/contact-info/contact-info'
-import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
-import emailjs from 'emailjs-com';
+import NavBar from "../components/navbar/Navbar";
+import Contact from "../components/contact/contact";
+import ContactInfo from "../components/contact-info/contact-info";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+import emailjs from "emailjs-com";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    '& > *': {
+    "& > *": {
       margin: theme.spacing(1),
-      width: '25ch',
+      width: "25ch",
     },
     bigField: {
-      height: "50px"
-    }
+      height: "50px",
+    },
   },
 }));
 
@@ -21,60 +21,100 @@ export default function Contacts() {
   const classes = useStyles();
 
   function sendEmail(e) {
-    console.log('privet')
+    console.log("privet");
 
     e.preventDefault();
 
-    emailjs.sendForm('gmail', 'template_ryaiefk', e.target, 'user_0YNf5nft1MHdU745e4Uq1')
-      .then((result) => {
-        console.log(result.text);
-      }, (error) => {
-        console.log(error.text);
-      });
+    emailjs
+      .sendForm(
+        "service_bd05yec",
+        "template_ryaiefk",
+        e.target,
+        "user_0YNf5nft1MHdU745e4Uq1"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   }
 
   return (
-    <div id='contact'>
+    <div id="contact">
       <NavBar />
 
       <form className="contact-form" onSubmit={sendEmail}>
         <section>
-          <div className='c-container'>
-            <div className='contactInfo'>
+          <div className="c-container">
+            <div className="contactInfo">
               <div>
-                <h1 style={{ marginBottom: '20px' }}>Contact info</h1>
+                <h1 style={{ marginBottom: "20px" }}>Contact info</h1>
                 <ContactInfo />
               </div>
-              <div id='info'>
+              <div id="info">
                 <Contact />
               </div>
             </div>
-            <div className='contactForm'>
+            <div className="contactForm">
               <h1>Send a Message</h1>
 
-              <div className='imputs'>
-                <div className='i-top'>
-                  <TextField id="name" name='name' required label="Name" variant="standard" style={{
-                    width: '100%', marginBottom: '20px'
-                  }} />
+              <div className="imputs">
+                <div className="i-top">
+                  <TextField
+                    id="name"
+                    name="name"
+                    required
+                    label="Name"
+                    variant="standard"
+                    style={{
+                      width: "100%",
+                      marginBottom: "20px",
+                    }}
+                  />
                   <div className="calhoz"></div>
-                  <TextField id="email" name='email' required label="Email" variant="standard" style={{
-                    width: '100%', marginBottom: '20px'
-                  }} />
+                  <TextField
+                    id="email"
+                    name="email"
+                    required
+                    label="Email"
+                    variant="standard"
+                    style={{
+                      width: "100%",
+                      marginBottom: "20px",
+                    }}
+                  />
                 </div>
-                <TextField id="standard-basic" name='subject' required label="Subject" variant="standard" style={{ marginBottom: '20px' }} />
-                <TextField id="standard-basic" name='message' className={classes.bigField} multiline maxRows={5} required label="Message" variant="standard"
+                <TextField
+                  id="standard-basic"
+                  name="subject"
+                  required
+                  label="Subject"
+                  variant="standard"
+                  style={{ marginBottom: "20px" }}
+                />
+                <TextField
+                  id="standard-basic"
+                  name="message"
+                  className={classes.bigField}
+                  multiline
+                  maxRows={5}
+                  required
+                  label="Message"
+                  variant="standard"
                   style={{
-                    height: '25px'
+                    height: "25px",
                   }}
                 />
               </div>
 
-              <input type="submit" value="Send" className='send-message' />
+              <input type="submit" value="Send" className="send-message" />
             </div>
           </div>
         </section>
       </form>
     </div>
-  )
+  );
 }
